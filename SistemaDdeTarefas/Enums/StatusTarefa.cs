3ae47452bd3b97f3eb075be0d9f2c562db0e6a82
MendesEduardo <eduardomn2018@gmail.com>
2023-10-21 21:42:38 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDdeTarefas/Enums/StatusTarefa.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace SistemaDdeTarefas.Enums
+{
+    public enum StatusTarefa
+    {
+        [Description("A fazer")]
+        AFazer = 1,
+        [Description("Em Andamento")]
+        EmAndamento = 2,
+        [Description("Cl=oncluido")]
+        Concluido = 3,
+    }
+}
